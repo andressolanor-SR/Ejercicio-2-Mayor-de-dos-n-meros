225aba1ddeda0b9a8df3a2492f3966bfb643356d
andressolanor-SR <andres.solanor@unmsm.edu.pe>
2026-05-11 08:45:15 -0500

    Ejercicio 2: Mayor de dos números
    Enunciado: Lee dos números enteros y determina cuál es mayor o si son iguales.
    
    Sugerencia: Compara A > B, luego B > A.

---
+++ # Leer dos números enteros.py
@@ -0,0 +1,11 @@
+# Leer dos números enteros
+A = int(input("Ingresa el primer número: "))
+B = int(input("Ingresa el segundo número: "))
+
+# Comparar los números
+if A > B:
+    print("El mayor es:", A)
+elif B > A:
+    print("El mayor es:", B)
+else:
+    print("Los números son iguales")
